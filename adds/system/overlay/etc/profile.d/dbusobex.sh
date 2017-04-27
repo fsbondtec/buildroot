bbec3c795959509366c9fb4dc526adb6730b86c1
Christian Reisenberger <christian.reisenberger@asoss.at>
2017-04-27 13:58:31 +0200

    add obexd dbus autostart script - is runned at login, change bt-device name

--- adds/system/overlay/etc/profile.d/dbusobex.sh
+++ adds/system/overlay/etc/profile.d/dbusobex.sh
@@ -1,3 +1,10 @@
-#start dbus
-export `dbus-launch | head -n 1`
-/usr/libexec/bluetooth/obexd &
+#check if dbus is started if not, start it! - is done with tmp file
+if [ ! -f "/tmp/dbus-started" ]; then
+	dbus-launch --sh-syntax > /tmp/dbus-started
+	eval `cat /tmp/dbus-started`
+	/usr/libexec/bluetooth/obexd &
+else 
+	#set only environment variables
+	eval `cat /tmp/dbus-started`
+fi
+
